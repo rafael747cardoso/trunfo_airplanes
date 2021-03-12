c540ebfcb7e9795842d05a53fb0383a50fd1e937
rafael747cardoso <rafael747cardoso@gmail.com>
2021-03-12 20:49:24 -0300

    data table and filter finished

--- Funcs/ui_table.py
+++ Funcs/ui_table.py
@@ -3,13 +3,15 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-def tab_table(vars_poss_num,
+def tab_table(vars_poss_filter_num,
+              vars_poss_filter_cat,
               filter_operations_poss
               ):
     """
     Makes the UI of the Table tab.
-    :param vars_poss_num:
     :param filter_operations_poss:
+    :param vars_poss_filter_num:
+    :param vars_poss_filter_cat:
     :return: 
     """
 
@@ -25,41 +27,102 @@ def tab_table(vars_poss_num,
                                     # Filters:
                                     dbc.Row(
                                         [
+                                            # Numeric:
                                             dbc.Col(
                                                 [
-                                                    dbc.Select(
-                                                        id = "table_filter_var_name",
-                                                        options = vars_poss_num,
-                                                        value = vars_poss_num[0]["value"]
+                                                    dbc.Card(
+                                                        [
+                                                            dbc.Row(
+                                                                [
+                                                                    dbc.Col(
+                                                                        [
+                                                                            html.H4("Numeric Filter"),
+                                                                            html.Br()
+                                                                        ],
+                                                                        width = 12
+                                                                    ),
+                                                                    dbc.Col(
+                                                                        [
+                                                                            dbc.Select(
+                                                                                id = "table_filter_num_var_name",
+                                                                                options = vars_poss_filter_num,
+                                                                                value = vars_poss_filter_num[0]["value"]
+                                                                            )
+                                                                        ],
+                                                                        width = 5
+                                                                    ),
+                                                                    dbc.Col(
+                                                                        [
+                                                                            dbc.Select(
+                                                                                id = "table_filter_num_operation",
+                                                                                options = filter_operations_poss,
+                                                                                value = filter_operations_poss[0]["value"]
+                                                                            )
+                                                                        ],
+                                                                        width = 3
+                                                                    ),
+                                                                    dbc.Col(
+                                                                        [
+                                                                            dbc.Input(
+                                                                                id = "table_filter_num_var_value",
+                                                                                type = "number",
+                                                                                value = 0
+                                                                            )
+                                                                        ],
+                                                                        width = 4
+                                                                    )
+                                                                ]
+                                                            )
+                                                        ],
+                                                        className = "plot-card",
+                                                        inverse = True
                                                     )
                                                 ],
-                                                width = 4
+                                                width = 6
                                             ),
+                                            
+                                            # Categoric:
                                             dbc.Col(
                                                 [
-                                                    dbc.Select(
-                                                        id = "table_filter_operation",
-                                                        options = filter_operations_poss,
-                                                        value = filter_operations_poss[0]["value"]
+                                                    dbc.Card(
+                                                        [
+                                                            dbc.Row(
+                                                                [
+                                                                    dbc.Col(
+                                                                        [
+                                                                            html.H4("Categoric Filter"),
+                                                                            html.Br()
+                                                                        ],
+                                                                        width = 12
+                                                                    ),
+                                                                    dbc.Col(
+                                                                        [
+                                                                            dbc.Select(
+                                                                                id = "table_filter_cat_var_name",
+                                                                                options = vars_poss_filter_cat,
+                                                                                value = vars_poss_filter_cat[0]["value"]
+                                                                            )
+                                                                        ],
+                                                                        width = 6
+                                                                    ),
+                                                                    dbc.Col(
+                                                                        [
+                                                                            dbc.Select(
+                                                                                id = "table_filter_cat_var_value",
+                                                                                options = [{"label": "", "value": ""}],
+                                                                                value = ""
+                                                                            )
+                                                                        ],
+                                                                        width = 6
+                                                                    )
+                                                                ]
+                                                            )
+                                                        ],
+                                                        className = "plot-card",
+                                                        inverse = True
                                                     )
                                                 ],
-                                                width = 2
-                                            ),
-                                            dbc.Col(
-                                                [
-                                                    dbc.Input(
-                                                        id = "table_filter_var_value",
-                                                        type = "number",
-                                                        value = 0
-                                                    )
-                                                ],
-                                                width = 3
-                                            ),
-                                            dbc.Col(
-                                                [
-                                                    
-                                                ],
-                                                width = {"size": 2, "offset": 1}
+                                                width = 6
                                             )
                                         ]
                                     ),
--- app.py
+++ app.py
@@ -91,7 +91,8 @@ labels_cat = {
 }
 
 # Possible variables for the selects:
-vars_poss_num = [
+vars_poss_filter_num = [
+    {"label": "Variable", "value": "Variable"},
     {"label": "Thrust", "value": "thrust_kN"},
     {"label": "Max Takeoff Mass", "value": "max_takeoff_mass_kg"},
     {"label": "Speed", "value": "speed_kmh"},
@@ -101,7 +102,9 @@ vars_poss_num = [
     {"label": "Height", "value": "height_m"},
     {"label": "Wingspan", "value": "wing_span_m"}
 ]
-vars_poss_cat = [
+vars_poss_num = vars_poss_filter_num[1:]
+vars_poss_filter_cat = [
+    {"label": "Variable", "value": "Variable"},
     {"label": "Manufacturer Name", "value": "manufacturer_name"},
     {"label": "Manufacturer Country", "value": "manufacturer_country"},
     {"label": "Engine Mount", "value": "engine_mount"},
@@ -109,6 +112,7 @@ vars_poss_cat = [
     {"label": "Wing Config", "value": "wing_config"},
     {"label": "Main Operator", "value": "main_operator"}
 ]
+vars_poss_cat = vars_poss_filter_cat[1:]
 vars_poss_dens_cat = [
     {"label": "All categories", "value": "all_categories"},
     {"label": "Main Operator", "value": "main_operator"},
@@ -123,6 +127,7 @@ funcs_pie_poss = [
     {"label": "Maximum", "value": "max"},
 ]
 filter_operations_poss = [
+    {"label": "Operator", "value": "Operator"},
     {"label": ">=", "value": ">="},
     {"label": ">", "value": ">"},
     {"label": "==", "value": "=="},
@@ -214,7 +219,7 @@ def update_plot_density_eda(x_density_eda,
     [Input(component_id = "cat_pie_count_eda", component_property = "value")]
 )
 def update_plot_pie_count_eda(cat_pie_count_eda):
-    df = df_airplanes.groupby(cat_pie_count_eda)[cat_pie_count_eda].count()
+    df = df_airplanes.copy().groupby(cat_pie_count_eda)[cat_pie_count_eda].count()
     levels_names = list(df.index)
     levels_counts = list(df)    
     plot_pie_count_eda = go.Figure(
@@ -240,13 +245,13 @@ def update_plot_pie_count_eda(x_pie_func_eda,
                               function_pie_func_eda,
                               cat_pie_func_eda):
     if function_pie_func_eda == "mean":
-        df = df_airplanes.groupby(cat_pie_func_eda)[x_pie_func_eda].mean().astype(int)
+        df = df_airplanes.copy().groupby(cat_pie_func_eda)[x_pie_func_eda].mean().astype(int)
     if function_pie_func_eda == "sum":
-        df = df_airplanes.groupby(cat_pie_func_eda)[x_pie_func_eda].sum().astype(int)
+        df = df_airplanes.copy().groupby(cat_pie_func_eda)[x_pie_func_eda].sum().astype(int)
     if function_pie_func_eda == "min":
-        df = df_airplanes.groupby(cat_pie_func_eda)[x_pie_func_eda].min().astype(int)
+        df = df_airplanes.copy().groupby(cat_pie_func_eda)[x_pie_func_eda].min().astype(int)
     if function_pie_func_eda == "max":
-        df = df_airplanes.groupby(cat_pie_func_eda)[x_pie_func_eda].max().astype(int)
+        df = df_airplanes.copy().groupby(cat_pie_func_eda)[x_pie_func_eda].max().astype(int)
     levels_names = list(df.index)
     levels_counts = list(df)    
     plot_pie_func_eda = go.Figure(
@@ -286,20 +291,44 @@ def update_plot_parallel_sets_eda(dimensions_parallel_sets_eda):
 
 #################### Table
 
+# Update the options for the categorical filter select:
+@app.callback(
+    Output(component_id = "table_filter_cat_var_value", component_property = "options"),
+    [Input(component_id = "table_filter_cat_var_name", component_property = "value")]
+)
+def update_select_filter_cat(table_filter_cat_var_name):
+    opt0 = [{"label": "", "value": ""}]
+    if table_filter_cat_var_name in df_airplanes.columns:
+        opts = [{"label": l, "value": l} for l in df_airplanes[table_filter_cat_var_name].unique()]
+    else:
+        opts = [{"label": "", "value": ""}]
+    return(opt0 + opts)
+
+# Table with filters:
 @app.callback(
     Output(component_id = "div_table", component_property = "children"),
-    [Input(component_id = "table_filter_var_name", component_property = "value"),
-     Input(component_id = "table_filter_operation", component_property = "value"),
-     Input(component_id = "table_filter_var_value", component_property = "value")
+    [Input(component_id = "table_filter_num_var_name", component_property = "value"),
+     Input(component_id = "table_filter_num_operation", component_property = "value"),
+     Input(component_id = "table_filter_num_var_value", component_property = "value"),
+     Input(component_id = "table_filter_cat_var_name", component_property = "value"),
+     Input(component_id = "table_filter_cat_var_value", component_property = "value")
      ]
 )   
-def update_table(table_filter_var_name,
-                 table_filter_operation,
-                 table_filter_var_value
-                 ):
-    # Filter:
-    op_func = ops[table_filter_operation]
-    df = df_airplanes[op_func(df_airplanes[table_filter_var_name], table_filter_var_value)]
+def update_table(table_filter_num_var_name,
+                 table_filter_num_operation,
+                 table_filter_num_var_value,
+                 table_filter_cat_var_name,
+                 table_filter_cat_var_value):
+    # Filters:
+    df = df_airplanes.copy()
+    if table_filter_num_var_name != "Variable" and \
+       table_filter_num_operation != "Operator":
+        op_func = ops[table_filter_num_operation]
+        df = df[op_func(df[table_filter_num_var_name], table_filter_num_var_value)]
+    if table_filter_cat_var_name != "Variable" and \
+       table_filter_cat_var_value != "Levels" and \
+       table_filter_cat_var_value != "":
+        df = df[df[table_filter_cat_var_name] == table_filter_cat_var_value]
     df.columns = nice_names
     
     # Table:
@@ -328,9 +357,12 @@ def update_table(table_filter_var_name,
     )
     return(table)
 
-
-
 #################### ML Models
+
+
+
+
+
 
 
 
@@ -363,7 +395,8 @@ app.layout = html.Div(
                 ),
                 dbc.Tab(
                     label = "Table",
-                    children = tab_table(vars_poss_num = vars_poss_num,
+                    children = tab_table(vars_poss_filter_num = vars_poss_filter_num,
+                                         vars_poss_filter_cat = vars_poss_filter_cat,
                                          filter_operations_poss = filter_operations_poss)
                 ),
                 dbc.Tab(
